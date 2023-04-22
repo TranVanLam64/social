5f188396e14a786acaa742ac4260b060c119ae9c
TranVanLam64 <vanlam8526@gmail.com>
2023-04-22 17:03:38 +0700

    call api post

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,7 +2,6 @@ import Post from "../post/Post";
 import "./posts.scss";
 import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios.js";
-
 const Posts = () => {
   const { isLoading, error, data } = useQuery(
     ["posts"],
@@ -16,9 +15,13 @@ const Posts = () => {
 
   return (
     <div className="posts">
-      {/* {data.map((post) => {
-        return <Post post={post} key={post.id} />;
-      })} */}
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? "Loading"
+        : data.map((post) => (
+            <Post post={post} key={post.id} />
+          ))}
     </div>
   );
 };
--- src/layouts/home/Home.jsx
+++ src/layouts/home/Home.jsx
@@ -1,11 +1,13 @@
 import Posts from "../../components/posts/Posts";
 import Stories from "../../components/stories/Stories";
 import "./home.scss";
+import Share from "../../components/share/Share";
 
 const Home = () => {
   return (
     <div className="home">
       <Stories />
+      <Share />
       <Posts />
     </div>
   );
